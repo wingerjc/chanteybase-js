374e3f837ef0f0ea2543196044a447aedb0d07a8
Jaymes Winger <rathmere@gmail.com>
2019-08-02 18:43:00 -0500

    Setup rendering fields from obj.

--- svelte/src/views/chantey.svelte
+++ svelte/src/views/chantey.svelte
@@ -2,15 +2,31 @@
   import abcjs from 'abcjs';
   import { onMount } from 'svelte';
 
-  let id = 'abc-123';
-  const song = 'X:1\nT:Speed the Plough\nM:4/4\nC:Trad.\nK:G\n|:GABc dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|\nGABc dedB|dedB dedB|c2ec B2dB|A2F2 G4:|\n|:g2gf gdBd|g2f2 e2d2|c2ec B2dB|c2A2 A2df|\ng2gf g2Bd|g2f2 e2d2|c2ec B2dB|A2F2 G4:|';
-  let data = {
-    "Title": { id: null, value: 'title'},
-    "Notation": { id: 'notation', value: null},
+  function Field(id, expander, value) {
+    this.id = id;
+    this.expander = expander;
+    this.value = value;
+  }
+
+  function extractField(fieldName) {
+    return (s) => {return s[fieldName] || "[not defined]"}
+  }
+
+  const DATA_FIELDS = {
+    "Title": new Field(null, false, extractField("title")),
+    "Notation": new Field("notation", true, (s) => {"[undefined]"}),
+    "ABC Notation": new Field(null, true, extractField("abc"))
   };
-  //setTimeout(function() {abcjs.renderAbc('notation', song, {})}, 300);
+
+  const dataObj = {
+    title: "the_title",
+    abc: 'X:1\nT:Speed the Plough\nM:4/4\nC:Trad.\nK:G\n|:GABc dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|\nGABc dedB|dedB dedB|c2ec B2dB|A2F2 G4:|\n|:g2gf gdBd|g2f2 e2d2|c2ec B2dB|c2A2 A2df|\ng2gf g2Bd|g2f2 e2d2|c2ec B2dB|A2F2 G4:|',
+  }
+
   onMount(() => {
-    abcjs.renderAbc('notation', song, {});
+    if(dataObj.abc && dataObj.abc.length > 0) {
+      abcjs.renderAbc('notation', dataObj.abc, {});
+    }
   })
 </script>
 
@@ -21,19 +37,16 @@
 
 </style>
 
-
-<div {id} class='stronger'> My ID is {id}</div>
-
 <section>
   <table border="2">
     <tr>
       <th>Field</th>
       <th>Data</th>
     </tr>
-    {#each Object.keys(data) as key}
+    {#each Object.keys(DATA_FIELDS) as key}
       <tr>
         <td class="field-title">{key}</td>
-        <td class="field-value" id={data[key].id}>{@html data[key].value || "[undefined]"}</td>
+        <td class="field-value" id={DATA_FIELDS[key].id}>{DATA_FIELDS[key].value(dataObj)}</td>
       </tr>
     {/each}
   </table>
