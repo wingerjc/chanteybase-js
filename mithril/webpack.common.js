1db9770936de465a572375e0c9f03f81f95a587d
Jaymes Winger <rathmere@gmail.com>
2021-09-02 21:59:15 -0500

    Stub mithril site.

---
+++ mithril/webpack.common.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'bin'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Chanteybase",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      }
+    ],
+  },
+};
